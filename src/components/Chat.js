54f8c3617ac620822301948e1f3f69ccc2c280b2
corentin.leb <leberre.corentin@gmail.com>
2021-03-04 14:20:53 +0100

    adding style finishing chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,18 +7,18 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
 
  const Chat = () => {
     const messagesRef = db.collection('messages');
-    const query = messagesRef.orderBy('createdAt').limitToLast();
+    const query = messagesRef.orderBy('createdAt').limitToLast(25);
 
     const [messages] = useCollectionData(query, {idField : 'id'});
     const [formValue, setFormValue] = useState('');
 
     const sendMessage = async (e) => {
         e.preventDefault();
-        const {uid, photoURL} = auth().currentUser;
+        const {uid, photoURL, displayName} = auth().currentUser;
         await messagesRef.add({
             text: formValue,
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-            uid, photoURL
+            uid, photoURL, displayName
         })
 
         setFormValue('');
@@ -26,9 +26,9 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
     return (
         <>
         <main>
-            {messages && messages.map((msg)=>{
+            {messages && messages.map((msg)=>
                 <ChatMessage key={msg.id} message={msg}/>
-            })}
+            )}
         </main>
         <form onSubmit={sendMessage}>
             <input type="text" value={formValue} onChange={(e)=>setFormValue(e.target.value)} placeholder="Say something salty"/>
--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { auth } from '../services/firebase';
 
 const ChatMessage = ({message}) => {
-    const { text, uid, photoURL } = message;
+    const { text, uid, photoURL, displayName } = message;
+
+    const messageClass = uid === auth().currentUser.uid ? 'sent' : 'received'
     return (
-        <div className="message">
-            <img src="{photoURL}" alt="User photo"/>
+        <div className={`message ${messageClass}`}>
+            <img src={photoURL} alt="User photo"/>
+            <div>
+            <p>{displayName}</p>
             <p>{text}</p>
+            </div>  
         </div>
     )
 }
